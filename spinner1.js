61e1d02289e0c7e4d9be0b4f9f4f0fbccad74887
TJ-Blinn <tjblinn@hotmail.com>
2021-04-07 03:41:39 +0000

    SpinnerI

---
+++ spinner1.js
@@ -0,0 +1,29 @@
+process.stdout.write("hello from spinner1.js... \rheyyy\n");
+
+// heyyy from spinner1.js...
+// The first few characters are replaced by "heyyy". This is because \r brought our cursor back to the beginning.
+
+setTimeout(() => {
+  process.stdout.write("\r|   ");
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write("\r/   ");
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write("\r-   ");
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write("\r\\   ");
+}, 700);
+
+setTimeout(() => {
+  process.stdout.write("\r|   ");
+}, 900);
+
+setTimeout(() => {
+  process.stdout.write("\r/   ");
+}, 1100);
